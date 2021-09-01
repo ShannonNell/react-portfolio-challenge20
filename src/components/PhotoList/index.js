7c7f9f0c1e4c3d20a7328c198c4290200df07dec
Shannon <shannon.nell92@gmail.com>
2021-09-01 14:43:10 -0400

    PhotoList seeded with project data

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -13,6 +13,41 @@ const PhotoList = ({ category }) => {
             deployedApp: 'https://uotgroup3.github.io/beats-and-bops/',
             skills: 'HTML, CSS, JavaScript, APIs'
         },
+        {
+            name: 'Survey Donkey',
+            description: 'A survey application where users are able to make their own surveys and share with others.',
+            github: 'https://github.com/uotgroup1/project2-group1',
+            deployedApp: 'https://obscure-river-98687.herokuapp.com/',
+            skills: 'HTML, CSS, JavaScript, Node.js, Express.js'
+        },
+        {
+            name: 'Budget Tracker',
+            description: 'This app is a budget tracker that allows for offline access and functionality.',
+            github: 'https://github.com/ShannonNell/budget-tracker-challenge19',
+            deployedApp: 'https://tranquil-sea-72490.herokuapp.com/',
+            skills: 'HTML, CSS, JavaScript, IndexDB, Service Workers, Web Manifest'
+        },
+        {
+            name: 'Work Day Scheduler',
+            description: 'Calendar app that allows user to save events for each hour of the day.',
+            github: 'https://github.com/ShannonNell/UoT-coding-challenge-5',
+            deployedApp: 'https://shannonnell.github.io/UoT-coding-challenge-5/',
+            skills: 'HTML, CSS, JavaScript, jQuery, API, DOM'
+        },
+        {
+            name: 'Note Taker',
+            description: 'An application that can be sued to write, save, and delete notes.',
+            github: 'https://github.com/ShannonNell/UoT-coding-challenge-11',
+            deployedApp: 'https://mysterious-wave-31250.herokuapp.com/',
+            skills: 'JavaScript, Node.js, jQuery, Express.js'
+        },
+        {
+            name: 'Tech Blog',
+            description: `A CMS-style blog site where developers can publish blog posts and comment on other developer's posts.`,
+            github: 'https://github.com/ShannonNell/tech-blog-challenge14',
+            deployedApp: 'https://secret-inlet-01236.herokuapp.com/',
+            skills: 'Handlebars, Sequelize, Express.js, MVC, ORM, API'
+        },
     ]);
 
     const currentPhotos = photos.filter(photo => photo.category === category);
